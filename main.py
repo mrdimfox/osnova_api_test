2687e13de27b54842bd532e1102029b305da980d
mrdimfox <mrlisdim@gmail.com>
2020-10-10 23:18:36 +0000

    Get comment by id with no auth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from pprint import pprint
+from typing import Sequence, Any
 
 API_TOKEN = ''
 API_URL_BASE = 'https://api.digitalocean.com/v2/'
@@ -10,9 +11,17 @@ VERSION = "0.1.0"
 
 HEADERS = {'User-Agent':f'{APP_NAME}-app/{VERSION}',
            'X-Device-Token': '{0}'.format(API_TOKEN)}
+HEADERS_NO_AUTH = {'User-Agent':f'{APP_NAME}-app/{VERSION}'}
+
 
 API_VER = "1.8"
 API_URL = f'https://api.tjournal.ru/v{API_VER}'
+
+def _first(seq: Sequence[Any]):
+    return seq[0]
+
+def _unwrap_result(json_content: dict):
+    return json_content["result"]
 
 def main():
     user_id = 107101
@@ -22,14 +31,14 @@ def main():
     request = f"{API_URL}/entry/{entry_id}/comments/thread/{comment_id}"
     print(f"Request: {request}.")
 
-    resp = requests.get(url=f"{request}", headers=HEADERS)
+    resp = requests.get(url=f"{request}", headers=HEADERS_NO_AUTH)
 
     print(f"Responce code: {resp.status_code}")
 
     if resp.status_code == 200:
-        json_content = resp.json()
+        json_content = _unwrap_result(resp.json())
         print(f"Responce:")
-        pprint(json_content["result"]["items"][0])
+        pprint(_first(json_content["items"]))
 
 if __name__ == "__main__":
     main()
