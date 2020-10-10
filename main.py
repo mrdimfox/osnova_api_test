586f700c56c19e6cf13c3ad0b66486b3f5d52b45
mrdimfox <mrlisdim@gmail.com>
2020-10-10 23:07:54 +0000

    Extract the exact comment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from pprint import pprint
 
 API_TOKEN = ''
 API_URL_BASE = 'https://api.digitalocean.com/v2/'
@@ -14,13 +16,20 @@ API_URL = f'https://api.tjournal.ru/v{API_VER}'
 
 def main():
     user_id = 107101
-    request = f"{API_URL}/user/{user_id}"
+    entry_id = 221782
+    comment_id = 4263642
+
+    request = f"{API_URL}/entry/{entry_id}/comments/thread/{comment_id}"
     print(f"Request: {request}.")
 
-    resp = requests.get(url=f"{API_URL}/user/{user_id}", headers=HEADERS)
+    resp = requests.get(url=f"{request}", headers=HEADERS)
 
     print(f"Responce code: {resp.status_code}")
-    print(f"Responce: {resp.json()}")
+
+    if resp.status_code == 200:
+        json_content = resp.json()
+        print(f"Responce:")
+        pprint(json_content["result"]["items"][0])
 
 if __name__ == "__main__":
     main()
