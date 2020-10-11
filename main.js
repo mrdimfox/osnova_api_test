0488e3aafa322e6a85edbb54621371ac1e425a4c
mrdimfox <mrlisdim@gmail.com>
2020-10-11 01:41:58 +0000

    Multi comments request

--- main.js
+++ main.js
@@ -10,13 +10,24 @@ const VERSION = "0.1.0";
 HEADERS_NO_AUTH = { 'User-Agent': `${APP_NAME}-app/${VERSION}` };
 
 
-entry_id = 221782;
-comment_id = 4263642;
+const entry_id = 221782;
 
-request_url = `${API_URL}/entry/${entry_id}/comments/thread/${comment_id}`;
-console.log(`Request: ${request_url}.`);
+const TESTING_COMMENT_ID = 4263642;
 
-(async () => {
+let comment_ids = new Array(3);
+comment_ids.fill(TESTING_COMMENT_ID) // actually should be array of different ids
+
+let request_url_template = (comment_id) => `${API_URL}/entry/${entry_id}/comments/thread/${comment_id}`;
+
+/**
+ * Get comment from 221782 entry by id
+ * @param {number} comment_id
+ * @returns {Object} dict with comment content
+ */
+async function get_comment(comment_id) {
+    const request_url = request_url_template(comment_id);
+    console.log(`Request: ${request_url}.`);
+
     let get_resp_json = () => fetch(request_url, { headers: HEADERS_NO_AUTH })
         .then(res => res.ok ? res : Promise.reject(res))
         .then(response => response.json());
@@ -28,5 +39,12 @@ console.log(`Request: ${request_url}.`);
             return {}
         });
 
-    console.log(first_comment)
+    return first_comment;
+}
+
+(async () => {
+    let tasks = comment_ids.map(async (val) => { return get_comment(val) })
+    const comments = await Promise.all(tasks)
+
+    console.log(comments)
 })();
