3f7c675ecf97d9713ce54811401e6bb293eefc3c
mrdimfox <mrlisdim@gmail.com>
2020-10-11 00:51:23 +0000

    Add some async magic

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ const VERSION = "0.1.0";
 HEADERS_NO_AUTH = { 'User-Agent': `${APP_NAME}-app/${VERSION}` };
 
 
-user_id = 107101;
 entry_id = 221782;
 comment_id = 4263642;
 
@@ -18,12 +17,16 @@ request_url = `${API_URL}/entry/${entry_id}/comments/thread/${comment_id}`;
 console.log(`Request: ${request_url}.`);
 
 (async () => {
-    const responce = await fetch(request_url, { headers: HEADERS_NO_AUTH });
+    let get_resp_json = () => fetch(request_url, { headers: HEADERS_NO_AUTH })
+        .then(res => res.ok ? res : Promise.reject(res))
+        .then(response => response.json());
 
-    if (responce.ok) {
-        json_content = await responce.json();
-        json_content = json_content["result"];
+    let first_comment = await get_resp_json()
+        .then(json => json["result"]["items"][0])
+        .catch(function (error) {
+            console.log("Request failed!")
+            return {}
+        });
 
-        console.log(json_content["items"][0]);
-    }
+    console.log(first_comment)
 })();
